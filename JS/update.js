ad876caf4155adc1d703c29f6461b8f6e0be96b4
tricky7377 <rodrigo-rojo@hotmail.com>
2018-10-18 10:08:48 +0200

    Animaciones del faraón arregladas
    Ahora el faraón funciona con el motor de físicas matter y funcionan las animaciones :DDDD

--- JS/update.js
+++ JS/update.js
@@ -2,9 +2,9 @@
 
 function update(){
     const keys = this.keys;
-    p.resetColliding();
+   
 	p.update(keys); //Update of the pharaoh
     //m.update(keys); 	 //Update of the mummy
-
+     p.resetColliding();
 }
 
