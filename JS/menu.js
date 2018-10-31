d1b66294cd8181806ad42ba48e0fdbe9f339c4dd
Irene <inmusic.nazul@gmail.com>
2018-10-31 10:42:32 +0100

    botones activos
    botones con acciones ya adquiridas al hacer click sobre ellos

--- JS/menu.js
+++ JS/menu.js
@@ -3,26 +3,82 @@ var menu = new Phaser.Scene('Menu');
 menu.init = function(){}
 
 menu.preload = function(){
-	this.load.image('play','../Escape-Bandages/sprites/play.png');
-	//this.load.image('options','../Escape-Bandages/sprites/options.png')
+	//cargar imagenes
+	this.load.image('play','../Escape-Bandages/Sprites/play.png');
+	this.load.image('options','../Escape-Bandages/Sprites/options.png');
+	this.load.image('off', '../Escape-Bandages/Sprites/offline.png');
+	this.load.image('on', '../Escape-Bandages/Sprites/online.png');
+	this.load.image('back', '../Escape-Bandages/Sprites/back.png');
 }
 
 menu.create = function(){
-	//boton 'play'
+	/////////////////////BOTON PLAY/////////////////////////////////
+	//cargar boton Play
 	this.bplay = this.add.sprite(w/6, h/6, 'play').setInteractive();
+	//hacer boton visible
 	this.bplay.setAlpha(1);
+	//accion al hacer click sobre el boton Play
 	this.bplay.on('pointerdown', function(){
-		menu.scene.switch(offline);
-		//menu.bplay.setAlpha(0);
-	})
-	console.log(this.bplay);
-
-	/*/boton 'options'
-	this.bopt = this.add.sprite(w-100, h-100, 'options').setInteractive();
-	this.bopt.setAlpha(1);
-	this.bopt.on('pointerdown', function(){
+		//hacer botones invisibles
+		menu.bplay.setAlpha(0);
 		menu.bopt.setAlpha(0);
-	})*/
+		//hacer botones visibles
+		menu.boff.setAlpha(1);
+		menu.bon.setAlpha(1);
+		menu.bback.setAlpha(1);
+	})
+	//console.log(this.bplay);
+
+	/////////////////////BOTON OFFLINE//////////////////////////////
+	//cargar boton Offline
+	this.boff = this.add.sprite(w/2+150, h/2, 'off').setInteractive();
+	//hacer boton invisible
+	this.boff.setAlpha(0);
+	//accion al hacer click sobre el boton Offline
+	this.boff.on('pointerdown', function(){
+		//cambio de escena
+		menu.scene.switch(offline);
+	})
+
+	/////////////////////BOTON ONLINE//////////////////////////////
+	//cargar boton Online
+	this.bon = this.add.sprite(w/2-150, h/2, 'on').setInteractive();
+	//hacer boton invisible
+	this.bon.setAlpha(0);
+	//accion al hacer click sobre el boton Online
+	this.bon.on('pointerdown', function(){
+
+	})
+	
+	//////////////////////BOTON OPTIONS///////////////////////////////
+	//cargar boton Options
+	this.bopt = this.add.sprite(w-100, h-100, 'options').setInteractive();
+	//hacer boton visible
+	this.bopt.setAlpha(1);
+	//accion al hacer click sobre el boton Options
+	this.bopt.on('pointerdown', function(){
+		//hacer botones invisibles
+		menu.bopt.setAlpha(0);
+		menu.bplay.setAlpha(0);
+		//hacer botones visibles
+		menu.bback.setAlpha(1);
+	})
+
+	//////////////////////BOTON BACK////////////////////////////////
+	//cargar boton Back
+	this.bback = this.add.sprite(w-100, h-100, 'back').setInteractive();
+	//hacer boton invisible
+	this.bback.setAlpha(0);
+	//accion al hacer click sobre el boton Back
+	this.bback.on('pointerdown', function(){
+		//hacer botones invisibles
+		menu.boff.setAlpha(0);
+		menu.bon.setAlpha(0);
+		menu.bback.setAlpha(0);
+		//hacer botones visibles
+		menu.bplay.setAlpha(1);
+		menu.bopt.setAlpha(1);
+	})
 }
 
 menu.update = function(){
--- JS/offline.js
+++ JS/offline.js
@@ -3,12 +3,26 @@ var offline = new Phaser.Scene('Offline');
 offline.init = function(){}
 
 offline.preload = function(){
-	this.load.image('options','../Escape-Bandages/sprites/options.png')
+	this.load.tilemapTiledJSON('map','map.json');
+	this.load.image('tile', '../Escape-Bandages/Sprites/tileset.png');
 }
 
 offline.create = function(){
-	this.bopt = this.add.sprite(w-100, h-100, 'options').setInteractive();
-	this.bopt.setAlpha(1);
+	const {Engine, Bodies, World} = Phaser.Physics.Matter.Matter;
+    const engine = Engine.create();
+
+    //TileMap creation
+	const map = this.make.tilemap({key:'map', tileWidth: 120, tileHeight: 120});
+    //We add the tileSet to the tileMap
+	const tiles = map.addTilesetImage('tileset','tile');
+    //Extract a layer of tiles from the map (fron the JSON)
+    const bg= map.createDynamicLayer('Background', tiles, 0,0);
+	const layer = map.createDynamicLayer('Foreground',tiles,0,0);
+
+    //We take the collider property from the JSON and make it a Collision for layer in Phaser
+    layer.setCollisionByProperty({ collider: true });
+    this.matter.world.convertTilemapLayer(layer);
+    this.matter.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 }
 
 offline.update = function(){}
