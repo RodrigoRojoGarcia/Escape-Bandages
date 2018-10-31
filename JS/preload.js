1637a27dee48399d2d15bd254fb66038aed1e3ea
tricky7377 <rodrigo-rojo@hotmail.com>
2018-10-31 21:50:40 +0100

    cambio de la spritesheet del faraón
    cambio de prueba de la spritesheet del faraón, dibujo no definitivo

--- JS/preload.js
+++ JS/preload.js
@@ -7,7 +7,7 @@ function preload(){
 	//Loas the spriteSheet of the mummy
 	this.load.spritesheet("Mummy","../Escape-Bandages/Sprites/mummySprites2.png", {frameWidth: 100, frameHeight: 150});
 	//Load the spriteSheet of the pharaoh
-	this.load.spritesheet("Pharaoh","../Escape-Bandages/Sprites/mummySprites2.png", {frameWidth: 100, frameHeight: 150});
+	this.load.spritesheet("Pharaoh","../Escape-Bandages/Sprites/pharaohsprites.png", {frameWidth: 100, frameHeight: 150});
 	//Load the spriteSheet of the torches
 	this.load.spritesheet("torch","../Escape-Bandages/Sprites/torchspriteSheet.png",{frameWidth: 30, frameHeight: 95});
 
