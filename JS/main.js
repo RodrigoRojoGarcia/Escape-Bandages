f947c60f278f902e7753b8f661087e1d057416c7
Irene <inmusic.nazul@gmail.com>
2018-11-06 21:12:37 +0100

    victoria arreglada
    escena victoria rehecha y terminada

--- JS/main.js
+++ JS/main.js
@@ -19,7 +19,7 @@ var config = {
 		}
 		]
 	},
-	scene:[menu, offline, victoria, submenu]
+	scene:[menu, offline, victoria, gameover, submenu]
 };
 
 var game = new Phaser.Game(config);
--- JS/menu.js
+++ JS/menu.js
@@ -5,15 +5,12 @@ menu.preload = function(){
 	this.load.image('bgmenu','../Escape-Bandages/Sprites/bgmenu.png');
 	this.load.image('title','../Escape-Bandages/Sprites/title.png');
 	this.load.image('play','../Escape-Bandages/Sprites/play.png');
-	this.load.image('out','../Escape-Bandages/Sprites/salir.png');
 	this.load.image('options','../Escape-Bandages/Sprites/options.png');
 	this.load.image('back', '../Escape-Bandages/Sprites/back.png');
 }
 
 menu.create = function(){
-	var bground = this.add.image(-100, -20, 'bgmenu').setOrigin(0);
-	bground.scaleX += 1;
-	bground.scaleY += 1;
+	var bground = this.add.image(0, 0, 'bgmenu').setOrigin(0);
 /////////////////////BOTON TITLE//////////////////////////////
 	//cargar boton Title
 	this.btit = this.add.sprite(960, 200, 'title').setInteractive();
@@ -23,8 +20,8 @@ menu.create = function(){
 ////////////////////BOTON PLAY/////////////////////////////////
 	//cargar boton Play
 	this.bplay = this.add.sprite(400, 500, 'play').setInteractive();
-	this.bplay.scaleX -= 0.2;
-	this.bplay.scaleY -= 0.2;
+	this.bplay.scaleX -= 0.1;
+	this.bplay.scaleY -= 0.1;
 	//hacer boton visible
 	this.bplay.setAlpha(1);
 	//accion al poner el cursor sobre el boton Play
@@ -61,6 +58,7 @@ menu.create = function(){
 	})
 	//accion al hacer click sobre el boton Options
 	this.bopt.on('pointerdown', function(){
+		menu.scene.switch(victoria);
 		/*//hacer botones invisibles
 		menu.bopt.setAlpha(0);
 		menu.bplay.setAlpha(0);
@@ -68,28 +66,6 @@ menu.create = function(){
 		menu.btit.setAlpha(0);
 		//hacer botones visibles
 		menu.bback.setAlpha(1);*/
-	})
-
-//////////////////////BOTON SALIR///////////////////////////////
-	//cargar boton Salir
-	this.bout = this.add.sprite(400, 700, 'out').setInteractive();
-	this.bout.scaleX -= 0.2;
-	this.bout.scaleY -= 0.2;
-	//hacer boton visible
-	this.bout.setAlpha(1);
-	//accion al poner el cursor sobre el boton Salir
-	this.bout.on('pointerover', function(){
-		menu.bout.scaleX += 0.15;
-		menu.bout.scaleY += 0.15;
-	})
-	//accion al quitar el cursor del boton Salir
-	this.bout.on('pointerout', function(){
-		menu.bout.scaleX -= 0.15;
-		menu.bout.scaleY -= 0.15;
-	})
-	//accion al hacer click sobre el boton Salir
-	this.bout.on('pointerdown', function(){
-		menu.scene.switch(victoria);
 	})
 }
 
