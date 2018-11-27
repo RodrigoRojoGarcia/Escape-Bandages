08c7d9115197b42474bbec4bd99a2c840b105905
ErnestoJM <ernesjimenez@hotmail.com>
2018-11-28 00:16:42 +0100

    inputs añadidos para el login y el registro del usuario
    inputs añadidos para el login y el registro del usuario y cambios menores

--- ServerEB/EscapeBandages/target/classes/static/JS/online.js
+++ ServerEB/EscapeBandages/target/classes/static/JS/online.js
@@ -69,42 +69,122 @@ online.create = function(){
     
     //texto para indicar que elija nombre
     //this.add.text(800,350,'Elija nombre de usuario:',{font: '32px Power Clear', fill:'#ffffff'})
-    this.add.dynamicBitmapText(800, 350, 'font1', 'Elija nombre de usuario:', 32);
+    this.add.dynamicBitmapText(1300, 400, 'font1', 'Nombre de Usuario:', 32);
+    this.add.dynamicBitmapText(1300, 550, 'font1', 'Contrasena:', 32);
+    
+    this.add.dynamicBitmapText(295, 400, 'font1', 'Nombre de Usuario:', 32);
+    this.add.dynamicBitmapText(295, 550, 'font1', 'Contrasena:', 32);
     //introducir por teclado el nombre
     //var textEntry = this.add.text(800,450,'',{font: '32px Power Clear',fill: '#ffffff'})
     
     //boolean que indica cuando esta escribiendo
-    this.typing = false;
+    this.typing1 = false;
+    this.typing2 = false;
+    
+    this.typing3 = false;
+    this.typing4 = false;
     //Input manual
     var w = 420;
     var h = 100;
-    this.inputK = this.add.sprite(735 + w/2, 415 + h/2, 'input').setInteractive({ cursor: 'url(Sprites/cursor4.png), pointer' });
+    
+    this.inputK = this.add.sprite(1235 + w/2, 415 + h/2, 'input').setInteractive({ cursor: 'url(Sprites/cursor4.png), pointer' });
+    this.inputK2 = this.add.sprite(1235 + w/2, 565 + h/2, 'input').setInteractive({ cursor: 'url(Sprites/cursor4.png), pointer' });
+    
+    this.inputK3 = this.add.sprite(230 + w/2, 415 + h/2, 'input').setInteractive({ cursor: 'url(Sprites/cursor4.png), pointer' });
+    this.inputK4 = this.add.sprite(230 + w/2, 565 + h/2, 'input').setInteractive({ cursor: 'url(Sprites/cursor4.png), pointer' });
+    
     this.inputK.anims.play('manualInput');
+    this.inputK2.anims.play('manualInput');
+    
+    this.inputK3.anims.play('manualInput');
+    this.inputK4.anims.play('manualInput');
     //inK.anims.play('manualInput');
     
     this.inputK.on('pointerdown', function(){
-    	online.typing = true;
+    	online.typing1 = true;
     	online.inputK.anims.stop();
     	online.inputK.setTexture('input', 0);
 	})
 	//accion al quitar el cursor del boton Salir
 	this.inputK.on('pointerout', function(){
-		online.typing = false;
+		online.typing1 = false;
 		if(textEntry.text.length == 0){
 			online.inputK.anims.play('manualInput');
 		}
 	})
+	//input2
+	this.inputK2.on('pointerdown', function(){
+    	online.typing2 = true;
+    	online.inputK2.anims.stop();
+    	online.inputK2.setTexture('input', 0);
+	})
+	//accion al quitar el cursor del boton Salir
+	this.inputK2.on('pointerout', function(){
+		online.typing2 = false;
+		if(textEntry2nd.text.length == 0){
+			online.inputK.anims.play('manualInput');
+		}
+	})
+	//input3
+	this.inputK3.on('pointerdown', function(){
+    	online.typing3 = true;
+    	online.inputK3.anims.stop();
+    	online.inputK3.setTexture('input', 0);
+	})
+	//accion al quitar el cursor del boton Salir
+	this.inputK3.on('pointerout', function(){
+		online.typing3 = false;
+		if(textEntry3rd.text.length == 0){
+			online.inputK3.anims.play('manualInput');
+		}
+	})
+	//input4
+	this.inputK4.on('pointerdown', function(){
+    	online.typing4 = true;
+    	online.inputK4.anims.stop();
+    	online.inputK4.setTexture('input', 0);
+	})
+	//accion al quitar el cursor del boton Salir
+	this.inputK4.on('pointerout', function(){
+		online.typing4 = false;
+		if(textEntry4th.text.length == 0){
+			online.inputK4.anims.play('manualInput');
+		}
+	})
 	
-    var textEntry = this.add.dynamicBitmapText(750, 450, 'font1', '', 32);
+    var textEntry = this.add.dynamicBitmapText(1250, 450, 'font1', '', 32);
+    var textEntry2nd = this.add.dynamicBitmapText(1250, 600, 'font1', '', 32);
+    
+    var textEntry3rd = this.add.dynamicBitmapText(245, 450, 'font1', '', 32);
+    var textEntry4th = this.add.dynamicBitmapText(245, 600, 'font1', '', 32);
+    
     //habilitar teclado para introducir texto
     this.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     this.backSpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACEBACK);
     this.input.keyboard.on('keydown', function(event){
-    	if(event.keyCode === 8 && textEntry.text.length>0 && online.typing){
+    	if(event.keyCode === 8 && textEntry.text.length>0 && online.typing1){
     		textEntry.text = textEntry.text.substr(0,textEntry.text.length-1)
     		
-    	}else if(event.keyCode == 32 || (event.keyCode >=48 && event.keyCode <90) && online.typing){
+    	}else if(event.keyCode === 8 && textEntry2nd.text.length>0 && online.typing2){
+    		textEntry2nd.text = textEntry2nd.text.substr(0,textEntry2nd.text.length-1)
+    	
+    	}else if(event.keyCode === 8 && textEntry3rd.text.length>0 && online.typing3){
+    		textEntry3rd.text = textEntry3rd.text.substr(0,textEntry3rd.text.length-1)
+    	
+    	}else if(event.keyCode === 8 && textEntry4th.text.length>0 && online.typing4){
+    		textEntry4th.text = textEntry4th.text.substr(0,textEntry4th.text.length-1)
+    	}
+    	else if(event.keyCode == 32 || (event.keyCode >=48 && event.keyCode <90) && online.typing1){
     		textEntry.text += event.key
+    	}
+    	else if(event.keyCode == 32 || (event.keyCode >=48 && event.keyCode <90) && online.typing2){
+    		textEntry2nd.text += event.key
+    	}
+    	else if(event.keyCode == 32 || (event.keyCode >=48 && event.keyCode <90) && online.typing3){
+    		textEntry3rd.text += event.key
+    	}
+    	else if(event.keyCode == 32 || (event.keyCode >=48 && event.keyCode <90) && online.typing4){
+    		textEntry4th.text += event.key
     	}
     })
 
@@ -135,7 +215,7 @@ online.create = function(){
 	})
 
 	////////////////////BOTON LOGIN////////////////////////////////////
-	this.introUser = this.add.sprite(950, 750, 'login').setInteractive({ cursor: 'url(Sprites/cursor3.png), pointer' });
+	this.introUser = this.add.sprite(1450, 750, 'login').setInteractive({ cursor: 'url(Sprites/cursor3.png), pointer' });
 	this.introUser.scaleX -= 0.4;
 	this.introUser.scaleY -= 0.4;
 	//hacer boton visible
@@ -152,7 +232,7 @@ online.create = function(){
 	})
 	//accion al hacer click sobre el boton
 	this.introUser.on('pointerdown', function(){
-		if(textEntry.text.length>0){
+		if(textEntry.text.length>0 && textEntry2nd.text.length>0){
 			userNameValid = true;
 			var user = {
 				id: myUser.Id,
@@ -164,6 +244,7 @@ online.create = function(){
 			})
 			while(textEntry.text.length>0){
 				textEntry.text = textEntry.text.substr(0,textEntry.text.length-1)
+				
 			}
 			
 			online.time.addEvent({
@@ -173,6 +254,48 @@ online.create = function(){
 	        });
 		}
 	})
+	
+	////////////////////BOTON REGISTRAR////////////////////////////////////
+	this.registerUser = this.add.sprite(445, 750, 'login').setInteractive({ cursor: 'url(Sprites/cursor3.png), pointer' });
+	this.registerUser.scaleX -= 0.4;
+	this.registerUser.scaleY -= 0.4;
+	//hacer boton visible
+	this.registerUser.setAlpha(1);
+	//accion al poner el cursor sobre el boton
+	this.registerUser.on('pointerover', function(){
+		online.registerUser.scaleX += 0.15;
+		online.registerUser.scaleY += 0.15;
+	})
+	//accion al quitar el cursor del boton
+	this.registerUser.on('pointerout', function(){
+		online.registerUser.scaleX -= 0.15;
+		online.registerUser.scaleY -= 0.15;
+	})
+	//accion al hacer click sobre el boton
+	this.registerUser.on('pointerdown', function(){
+		if(textEntry3rd.text.length>0 && textEntry4th.text.length>0){
+			userNameValid = true;
+			var user = {
+				id: myUser.Id,
+				userName: textEntry3rd.text
+			}
+			myUser.setUserName(textEntry3rd.text)
+			updateUserName(user, function(){
+				userNameValid = false;
+			})
+			while(textEntry3rd.text.length>0){
+				textEntry3rd.text = textEntry3rd.text.substr(0,textEntry3rd.text.length-1)
+				
+			}
+			
+			online.time.addEvent({
+	            delay: 40,
+	            callback: online.isUserNameValid,
+	            callbackScope: online
+	        });
+		}
+	})
+	
 	//Mensaje que sale cuando metes un usuario registrado
 	this.isUserNameValid = function(){
 		if(userNameValid){
