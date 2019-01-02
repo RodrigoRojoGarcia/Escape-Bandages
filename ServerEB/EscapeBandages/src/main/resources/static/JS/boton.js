e583d929a281faaa3be086c3cac01772772e8e93
tricky7377 <rodrigo-rojo@hotmail.com>
2019-01-02 21:29:10 +0100

    UIButton creado
    clase UIButton creada medio funcional

--- ServerEB/EscapeBandages/src/main/resources/static/JS/boton.js
+++ ServerEB/EscapeBandages/src/main/resources/static/JS/boton.js
@@ -1,7 +1,36 @@
-function UIButton(scene, posX, posY, smth){
+function UIButton(scene, posX, posY, sprite, smth){
+	that = this
 	this.scene = scene
 	this.posX = posX
 	this.posY = posY
 	this.onclick = smth
+	
+	this.scene.butt = this.scene.add.sprite(posX, posY, sprite).setInteractive({cursor: 'url(Sprites/cursor3.png), pointer'})
+	
+	this.scene.butt.on('pointerover', function(){
+		that.scene.butt.scaleX += 0.15;
+		that.scene.butt.scaleY += 0.15;
+	})
+	
+	this.scene.butt.on('pointerout', function(){
+		that.scene.butt.scaleX -= 0.15;
+		that.scene.butt.scaleY -= 0.15;
+	})
+	
+	
+	this.scene.butt.on('pointerdown', this.onclick)
+	
+	this.setScale = function(scaleX, scaleY){
+		this.scene.butt.scaleX -= scaleX;
+		this.scene.butt.scaleY -= scaleY;
+	}
+	
+	this.show = function(){
+		this.scene.butt.setAlpha(1);
+	}
+	
+	this.hide = function(){
+		this.scene.butt.setAlpha(0)
+	}
 	
 }
--- ServerEB/EscapeBandages/src/main/resources/static/JS/menu.js
+++ ServerEB/EscapeBandages/src/main/resources/static/JS/menu.js
@@ -35,77 +35,38 @@ menu.create = function(){
 
 ////////////////////BOTON PLAY/////////////////////////////////
 	//cargar boton Play
-	this.bplay = this.add.sprite(400, 500, 'play').setInteractive({ cursor: 'url(Sprites/cursor3.png), pointer' });
-	this.bplay.scaleX -= 0.1;
-	this.bplay.scaleY -= 0.1;
-
-	//hacer boton visible
-	this.bplay.setAlpha(1);
-	//accion al poner el cursor sobre el boton Play
-	this.bplay.on('pointerover', function(){
-		menu.bplay.scaleX += 0.15;
-		menu.bplay.scaleY += 0.15;
-	})
-	//accion al quitar el cursor del boton Play
-	this.bplay.on('pointerout', function(){
-		menu.bplay.scaleX -= 0.15;
-		menu.bplay.scaleY -= 0.15;
-	})
-	//accion al hacer click sobre el boton Play
-	this.bplay.on('pointerdown', function(){
+	this.bplay = new UIButton(this, 400, 500, 'play', function(){
 		//cambio de escena a submenu
 		sendMessage("UwUs")
 		menu.scene.switch(submenu);
 	})
-	//console.log(this.bplay);
+	this.bplay.setScale(0.1, 0.1);
+	this.bplay.show();
+	
 	
 //////////////////////BOTON SILENCIO///////////////////////////////
-	//cargar boton Silencio
-	this.bnvol = this.add.sprite(1750, 950, 'novolumen').setInteractive({ cursor: 'url(Sprites/cursor3.png), pointer' });
-	//hacer boton visible
-	this.bnvol.setAlpha(1);
-	//accion al poner el cursor sobre el boton Silencio
-	this.bnvol.on('pointerover', function(){
-		menu.bnvol.scaleX += 0.15;
-		menu.bnvol.scaleY += 0.15;
-	})
-	//accion al quitar el cursor del boton Silencio
-	this.bnvol.on('pointerout', function(){
-		menu.bnvol.scaleX -= 0.15;
-		menu.bnvol.scaleY -= 0.15;
-	})
-	//accion al hacer click sobre el boton Silencio
-	this.bnvol.on('pointerdown', function(){
+	
+	this.bnvol = new UIButton(this, 1750, 950, 'novolumen', function(){
 		fx.resume();
 		//hacer botones invisibles
-		menu.bnvol.setAlpha(0);
+		menu.bnvol.hide();
 		//hacer botones visibles
-		menu.bvol.setAlpha(1);
+		menu.bvol.show();
 	})
+	
+	this.bnvol.show();
 
 //////////////////////BOTON VOLUMEN///////////////////////////////
-	//cargar boton Volumen
-	this.bvol = this.add.sprite(1750, 950, 'volumen').setInteractive({ cursor: 'url(Sprites/cursor3.png), pointer' });
-	//hacer boton visible
-	this.bvol.setAlpha(0);
-	//accion al poner el cursor sobre el boton Volumen
-	this.bvol.on('pointerover', function(){
-		menu.bvol.scaleX += 0.15;
-		menu.bvol.scaleY += 0.15;
-	})
-	//accion al quitar el cursor del boton Volumen
-	this.bvol.on('pointerout', function(){
-		menu.bvol.scaleX -= 0.15;
-		menu.bvol.scaleY -= 0.15;
-	})
-	//accion al hacer click sobre el boton Volumen
-	this.bvol.on('pointerdown', function(){
+	
+	this.bvol = new UIButton(this, 1750, 950, 'volumen', function(){
 		fx.pause();
 		//hacer botones invisibles
-		menu.bvol.setAlpha(0);
+		menu.bvol.hide();
 		//hacer botones visibles
-		menu.bnvol.setAlpha(1);
+		menu.bnvol.show();
 	})
+	this.bvol.hide()
+	
 }
 
 menu.update = function(){}
