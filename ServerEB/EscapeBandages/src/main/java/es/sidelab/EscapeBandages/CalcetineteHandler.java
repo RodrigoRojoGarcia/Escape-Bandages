daea6fee86c0ff09e31c74cfc45e8425d988a19f
tricky7377 <rodrigo-rojo@hotmail.com>
2019-01-03 18:31:06 +0100

    Ahora con envío mensaje a todas las sesiones
    envío a todas las sesiones

--- ServerEB/EscapeBandages/src/main/java/es/sidelab/EscapeBandages/CalcetineteHandler.java
+++ ServerEB/EscapeBandages/src/main/java/es/sidelab/EscapeBandages/CalcetineteHandler.java
@@ -1,5 +1,9 @@
 package es.sidelab.EscapeBandages;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -10,6 +14,9 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 
 
 public class CalcetineteHandler extends TextWebSocketHandler{
+	
+	
+	private Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
 	
 	private ObjectMapper mapper = new ObjectMapper();
 	
@@ -23,7 +30,22 @@ public class CalcetineteHandler extends TextWebSocketHandler{
 		ObjectNode node = mapper.createObjectNode();
 		node.put("UwU", uwu);
 		
-		session.sendMessage(new TextMessage(node.toString()));
 		
+		for(WebSocketSession s : sessions.values()) {
+			s.sendMessage(new TextMessage(node.toString()));
+		}
+		
+		
+		
+	}
+	
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		sessions.put(session.getId(), session);
+	}
+	
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status)throws Exception{
+		sessions.remove(session.getId());
 	}
 }
